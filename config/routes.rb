a4e8d38af503ceab52daed90a59095a196e74d1f
Victor Branger <victor.branger@gmail.com>
2020-11-10 12:11:51 +0100

    added font-awesome and bootstrap / added read one route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # list the tasks
+  get 'tasks', to: 'tasks#index'
+  # read one tasks
+  get "tasks/:id", to: "tasks#show", as: :task
 end
