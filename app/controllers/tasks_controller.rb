c914bb91278b840bb6ba8f5254a9d47358268630
Victor Branger <victor.branger@gmail.com>
2020-11-10 12:44:11 +0100

    adding a task post route implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,18 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
   # add new task
   get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
   # read one tasks
   get 'tasks/:id', to: 'tasks#show', as: :task
 
